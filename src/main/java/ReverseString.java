814378a06bee3e0984da7e4fea6ca3097f7358ab
vkjonnabhatla <vkjonnabhatla@gmail.com>
2018-11-06 18:11:21 +0530

    Update ReverseString.java

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -2,15 +2,12 @@ public class ReverseString {
 
     public static void main(String[] args){
 
-        int ij = (8 >> 1);
-
-        String str = "Nisum";
         StringBuffer sb = new StringBuffer("VenkataKri");
 
         char[] charArr = sb.toString().toCharArray();
         int count = sb.length() - 1;
 
-        for(int j = (sb.length()/2 - 1); j >= 0; j--){
+        for(int j = (sb.length()/2 - 1); j >= 0; j--){ //Dividing the length of the string and - 1. and just swap the characters to reverse so hat we can time complexity.
             int k = count -j;
             char ch1 = charArr[j];
             char ch2 = charArr[k];
@@ -18,16 +15,16 @@ public class ReverseString {
             charArr[k] = ch1;
         }
 
-        System.out.println(charArr.toString());
+        System.out.println(" Reverse string is :: "+new String(charArr));
+        System.out.println(" Reverse string is :: "+new String(charArr));
+        //display
 
-
-
-        StringBuffer str1 = sb.reverse();
+        /*StringBuffer str1 = sb.reverse();
         System.out.println("Reverse String is: "+ str1);
         char[] seq = str.toCharArray();
         for(int i = seq.length - 1; i >= 0; i--){
             sb.append(seq[i]);
         }
-        //System.out.println("Reverse String is: "+ sb.reverse());
+        //System.out.println("Reverse String is: "+ sb.reverse());*/
     }
 }
