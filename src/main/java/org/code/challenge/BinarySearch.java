016b03d517499918755817895510da57d295c73e
Venkata Krishna Jonnabhatla <vkjonnabhatla@gmail.com>
2020-03-08 17:53:20 -0400

    BinaySearch example program

--- src/main/java/org/code/challenge/BinarySearch.java
+++ src/main/java/org/code/challenge/BinarySearch.java
@@ -22,7 +22,7 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start <= end) {
+        while (start <= end)  {
             int mid = (start + end) / 2;
             if (searchElement == arr[mid]) {
                 return mid;
